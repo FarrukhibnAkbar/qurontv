51f6b124910882632b38d53b8e9c8fdcc39ce96c
Farrux <farruxismoilov33@gmail.com>
2021-11-11 09:28:12 +0500

    Create database

---
+++ services/database/init.sql
@@ -0,0 +1,31 @@
+create table pathways(
+	pathway_id serial not null primary key,
+	name varchar(128) not null
+);
+
+create table courses(
+	course_id serial not null primary key,
+	name varchar(128) not null,
+	is_active boolean default false,
+	created_at  timestamp with time zone default current_timestamp,
+	pathway_id int not null references pathway(pathway_id)
+);
+
+
+create table takeaways(
+	takeaway_id serial not null primary key,
+	content varchar(256) not null,
+	course_id int not null references courses(course_id)
+);
+
+create table chapters(
+	chapter_id serial not null primary key,
+	name varchar(128) not null,
+	course_id int not null references courses(course_id)
+);
+
+create table lessons(
+	lesson_id serial not null primary key,
+	name varchar(128) not null,
+	chapter_id int not null references chapters(chapter_id)
+);
